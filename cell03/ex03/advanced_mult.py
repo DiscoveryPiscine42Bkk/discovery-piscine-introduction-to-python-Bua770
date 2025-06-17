b97456992bea56c3f68814b1b9ca46f54ef68fc5
Bua770 <buachanok1234@gmail.com>
2025-06-17 09:35:46 +0000

    add advanced_mult.py

---
+++ cell03/ex03/advanced_mult.py
@@ -0,0 +1,6 @@
+for multipiler in range(11): # 11 is end
+    print(f"Table de {multipiler}:", end="")
+  
+    for number in range(11):
+        print(f" {multipiler * number}", end="")
+    print()
