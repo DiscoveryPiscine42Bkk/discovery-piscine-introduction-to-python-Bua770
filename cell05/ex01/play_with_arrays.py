30bc56fe3a1aa6a5721e4a858fafde4453842697
Bua770 <buachanok1234@gmail.com>
2025-06-18 04:36:46 +0000

    add play_with_arrays2.py

--- cell05/ex01/play_with_arrays.py
+++ cell05/ex01/play_with_arrays.py
@@ -1,4 +1,4 @@
-original_array = [2,8,9,48,8,22,-12,2]
-new_array = [x * 2 +2 for x in original_array]
-print("Origina array:", new_array)
-print("New array:", new_array)
+numbers = [2, 8, 9, 48, 8, 22, -12, 2]
+plus_two = [data + 2 for data in numbers]
+print(f'Original array: {numbers}')
+print(f'New array: {plus_two}')
