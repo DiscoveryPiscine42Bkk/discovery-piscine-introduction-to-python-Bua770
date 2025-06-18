23d0ef8196fb231f6dd950cbbf304911dee416f7
Bua770 <buachanok1234@gmail.com>
2025-06-18 09:46:38 +0000

    add string_are_arrays.py

--- cell05/ex12/string_are_arrays.py
+++ cell05/ex12/string_are_arrays.py
@@ -1,12 +1,16 @@
 import sys
+
 def main():
-    if len(sys.argv)!=2:
+    if len(sys.argv) != 2:
         print("none")
         return
-    text=sys.argv[1]
-    count_z=text.count('z')
-    if count_z==0:
+
+    text = sys.argv[1]
+    count_z = text.count('z')
+
+    if count_z == 0:
         print("none")
     else:
-        print('z'*count_z)
-    main()
+
+        print('z' * count_z)
+main()
